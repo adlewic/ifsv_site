803053255ee7ed978094105ce9248f3ab42ce1d9
Holkan <luah613@gmail.com>
2017-04-07 00:21:13 -0500

    industrias?

--- themes/ifsv-theme/archive-industrias.php
+++ themes/ifsv-theme/archive-industrias.php
@@ -10,11 +10,13 @@
 				while(have_posts()):
 					the_post();
 				?>
-					<li> 
-						<button>
-							<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
-							<?php the_title(); ?>
-						</button>
+					<li>
+						<a href="<?php the_permalink(); ?>">
+							<button>
+								<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
+								<?php the_title(); ?>
+							</button>
+						</a>
 					</li>
 			<?php 
 				endwhile;
@@ -27,55 +29,42 @@
 
 
 <!-- general layout start  -->
-<div id="general_layout">
-		<div id="industria_layout">
-			<div id="in_layout_head">
-				<div>		
-					<!-- industry name  -->
-					<h3> Aeroespacial y Defensa </h3>
-					<!-- subtitle  --> 
-					<h4> IFS permite la gestion con capacidad en tierra, mar y aire</h4>
-				</div>
-
-				<p>
-					Con el aumento de los retos de los mercados emergentes, las nuevas líneas de productos, nuevas tecnologías, nuevas regulaciones, nuevas exigencias y nuevas formas de hacer negocios - combinados con el crecimiento estratégico, adquisiciones / fusiones y desafíos de los modelos de costos del operador.
-				</p>
+<!-- <div id="general_layout">
+	<div id="industria_layout">
+		<div id="in_layout_head">
+			<div>		
+				<h3> Aeroespacial y Defensa </h3>
+				<h4> IFS permite la gestion con capacidad en tierra, mar y aire</h4>
 			</div>
 
-				<!-- main description  -->
-			
-		
-				<!-- type of bussineses  -->
-				<ul>
-					<li> <span>  ● </span> Fabricacion de Automoviles  </li>
-					<li> <span>  ● </span> Concecionarios de Automoviles </li>
-					<li> <span>  ● </span> Porveedores de automoviles  </li>
-				</ul>
-		
-		</div><!-- industria layout end-->
-
-
-
-		<div id="specifics">
-				<ul>
-				<?php	
-					while(have_posts()):
-						the_post();
-					?>
-						<li> 
-							<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
-							<h4> <?php the_title(); ?></h4>
-						</li>
-				<?php 
-					endwhile;
-					?>
-				</ul>
-		<?php
-			endif;
-			?>
+			<p>
+				Con el aumento de los retos de los mercados emergentes, las nuevas líneas de productos, nuevas tecnologías, nuevas regulaciones, nuevas exigencias y nuevas formas de hacer negocios - combinados con el crecimiento estratégico, adquisiciones / fusiones y desafíos de los modelos de costos del operador.
+			</p>
+		</div>
+		<ul>
+			<li> <span>  ● </span> Fabricacion de Automoviles  </li>
+			<li> <span>  ● </span> Concecionarios de Automoviles </li>
+			<li> <span>  ● </span> Porveedores de automoviles  </li>
+		</ul>
 	</div>
 
-<!-- wrapper -->
-</div>
+	<div id="specifics">
+		<ul>
+		<?php
+		/*
+			while(have_posts()):
+				the_post();
+			?>
+				<li> 
+					<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
+					<h4> <?php the_title(); ?></h4>
+				</li>
+		<?php 
+			endwhile;
+			*/
+			?>
+		</ul>
+	</div>
+</div> -->
 
 <?php get_footer(); ?>	
--- themes/ifsv-theme/single-industrias.php
+++ themes/ifsv-theme/single-industrias.php
@@ -1,2 +1,39 @@
-<?php get_header(); if(have_posts()):while(have_posts()):the_post();endwhile;endif;?>
-<?php get_footer(); ?>
+<?php 
+	get_header(); 
+		if(have_posts()):
+			while(have_posts()):
+				the_post();
+			?>
+	<!-- general layout start  -->
+			<div id="general_layout">
+				<div id="industria_layout">
+					<div id="in_layout_head">
+						<div>		
+							<h3> <?php the_title(); ?> </h3>
+							<h4></h4>
+						</div>
+						<p>
+							<?php the_content(); ?>
+						</p>
+					</div>
+					<ul>
+						<li> <span>  ● </span> Fabricacion de Automoviles  </li>
+						<li> <span>  ● </span> Concecionarios de Automoviles </li>
+						<li> <span>  ● </span> Porveedores de automoviles  </li>
+					</ul>
+				</div>
+
+				<div id="specifics">
+					<ul>
+						<li> 
+							<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
+							<h4> <?php the_title(); ?></h4>
+						</li>
+					</ul>
+				</div>
+			</div>
+<?php 
+			endwhile;	
+		endif;
+	get_footer(); 
+	?>
