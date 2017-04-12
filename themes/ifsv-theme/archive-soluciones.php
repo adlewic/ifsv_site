4f93d97a74ecc9f9e7fd8a647e4f177183d13af8
Imelda Alonso <adlewic@gmail.com>
2017-04-12 18:58:25 -0500

    adding css

--- themes/ifsv-theme/archive-soluciones.php
+++ themes/ifsv-theme/archive-soluciones.php
@@ -36,7 +36,7 @@
 					the_post();
 				?>
 					<li> 
-						<img class="g_icons" src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/g_icons/jet.svg"><br>
 							<h4> <?php the_title(); ?></h4>
 					</li>
 			<?php 
